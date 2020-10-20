b749ffa55845abcef71c5d110f64f20ffc85b8fb
Joel MacKenzie <joelmackenzie3@gmail.com>
2020-10-19 21:19:22 -0700

    Updates

---
+++ precache-manifest.4ff8c52091e99d306c5208e6c80f4081.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a1120e4eaf42eeb68b6e5ec2ba6db69",
+    "url": "/WorkoutTracker/index.html"
+  },
+  {
+    "revision": "3f0d68ae35740737fcb3",
+    "url": "/WorkoutTracker/static/css/main.bf24a208.chunk.css"
+  },
+  {
+    "revision": "fe9eac34f05b0925da71",
+    "url": "/WorkoutTracker/static/js/2.6683a41b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/WorkoutTracker/static/js/2.6683a41b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f0d68ae35740737fcb3",
+    "url": "/WorkoutTracker/static/js/main.8e617bf7.chunk.js"
+  },
+  {
+    "revision": "456b4a48dc6e8776a65e",
+    "url": "/WorkoutTracker/static/js/runtime-main.0b30d031.js"
+  }
+]);
